9121f2ae4d0428924dc62a7b15c699a17f08ab04
GIMENAGASPAR <gimenagaspar7@gmail.com>
2023-11-02 23:14:39 -0300

    solucion de errores

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,18 @@
 window.addEventListener("load", function(){
 
     let botonResumen=document.getElementById("botonResumen");
+    let botonBorrar=document.getElementById("botonBorrar");
     let cat=document.getElementById("categoria");
-    console.log(cat);
+
     let totalapagar=document.querySelector(".text-totalapagar")
     botonResumen.addEventListener("click", function(){
         if(cat.value == "Estudiante"){
             totalapagar.innerHTML+=200-(200*80)/100;
         }
-        else if(cat.value == "Trainee"){
+        if(cat.value == "Trainee"){
             totalapagar.innerHTML+=200-(200*50)/100;
         }
-        else if(cat.value=="Junior"){
+        if(cat.value=="Junior"){
             totalapagar.innerHTML+=200-(200*15)/100;
         }
     })
