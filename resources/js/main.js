48f18d8dd16e59d952bc55eef5ddf34f8a5190f5
GIMENAGASPAR <gimenagaspar7@gmail.com>
2023-12-11 21:27:52 -0300

    correccion de errores

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,15 +5,26 @@ window.addEventListener("load", function(){
     let cat=document.getElementById("categoria");
 
     let totalapagar=document.querySelector(".text-totalapagar")
-    botonResumen.addEventListener("click", function(){
-        if(cat.value == "Estudiante"){
-            totalapagar.innerHTML+=200-(200*80)/100;
-        }
-        if(cat.value == "Trainee"){
-            totalapagar.innerHTML+=200-(200*50)/100;
-        }
-        if(cat.value=="Junior"){
-            totalapagar.innerHTML+=200-(200*15)/100;
+
+    botonResumen.addEventListener("click", function(event){
+
+        if (cat.value == "Estudiante" || cat.value == "Trainee" || cat.value == "Junior"){
+            let resultado;
+            switch (cat.value){
+                case "Estudiante":
+                    resultado=200-(200*80)/100
+                break;
+                case "Trainee":
+                    resultado=200-(200*50)/100;
+                break;
+                case "Junior":
+                    resultado=200-(200*15)/100;
+
+                break;
+            }
+            totalapagar.innerText = "Total a pagar: $" + resultado 
+        }else{
+            totalapagar.innerText = "Cálculo imposible"
         }
     })
 })
